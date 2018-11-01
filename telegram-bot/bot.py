38fc72b78eaff4491667d0c6de8925c75d9960b8
Mikhail Korolev <korolm.spb@gmail.com>
2018-11-02 00:27:33 +0300

    kzn dsc jobs: add print for incoming messages

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -44,6 +44,7 @@ def help(bot, update):
 def echo(bot, update):
     """Echo the user message."""
     # update.message.reply_text(update.message.text)
+    print(update.message.text)
     if process_text(update.message.text):
         update.message.reply_text('👎')
     else:
